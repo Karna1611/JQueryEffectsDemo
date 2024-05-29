ff1a33b0ef16c48e217a2803ab6f3226aaadbb81
Karna1611 <trivedi.karna@outlook.com>
2024-05-29 17:57:16 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JQueryEffectsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
